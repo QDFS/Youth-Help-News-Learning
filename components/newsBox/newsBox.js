6114776a7d99bc4fbac0db2c957c24e1c0988992
QDFS <2485066081@qq.com>
2023-11-02 21:05:10 +0800

    青年帮新闻学习项目

---
+++ components/newsBox/newsBox.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["components/newsBox/newsBox"],{"001e":function(e,t,n){"use strict";n.r(t);var u=n("e290"),c=n("81ca");for(var a in c)["default"].indexOf(a)<0&&function(e){n.d(t,e,(function(){return c[e]}))}(a);n("431c");var r=n("f0c5"),i=Object(r["a"])(c["default"],u["b"],u["c"],!1,null,"da3c52c4",null,!1,u["a"],void 0);t["default"]=i.exports},"115a":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var u={name:"newsBox",props:{item:{type:Object,default:function(){return{title:"背景板附件上半场定时VB城市低保是匆匆地SKV从必考eeeeeeeeeee",picurl:"../../static/images/0.jpg",author:"test1",hits:999}}}},data:function(){return{}}};t.default=u},"431c":function(e,t,n){"use strict";var u=n("80e9"),c=n.n(u);c.a},"80e9":function(e,t,n){},"81ca":function(e,t,n){"use strict";n.r(t);var u=n("115a"),c=n.n(u);for(var a in u)["default"].indexOf(a)<0&&function(e){n.d(t,e,(function(){return u[e]}))}(a);t["default"]=c.a},e290:function(e,t,n){"use strict";n.d(t,"b",(function(){return u})),n.d(t,"c",(function(){return c})),n.d(t,"a",(function(){}));var u=function(){var e=this.$createElement;this._self._c},c=[]}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/newsBox/newsBox-create-component',
+    {
+        'components/newsBox/newsBox-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('543d')['createComponent'](__webpack_require__("001e"))
+        })
+    },
+    [['components/newsBox/newsBox-create-component']]
+]);
